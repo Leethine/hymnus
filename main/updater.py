72a4fc72c8c1541182ac595a0055982e76ec3a9e
Z L <zian.li.1996@outlook.com>
2023-11-17 15:08:43 +0100

    Add front pages

---
+++ main/updater.py
@@ -0,0 +1,8 @@
+import os, sys
+
+#example_collection = '<tr> <td> <a href="#" target="_blank">Violin Sonatas and Partitas</a></td> <td>Johan Sebastian <b>Bach</b></td> </tr>'
+#example_composer = '<tr> <td><a href="#" target="_blank">Johann Sebastian <b>Bach</b></a></td> <td>1685 - 1750</td> </tr>'
+
+collection_new_row = '<tr><td><a href="::LINK::" target="_blank">::TITLE::</a></td> <td>::FIRSTNAME:: <b>::LASTNAME::</b></td></tr>'
+composer_new_row = '<tr><td><a href="::LINK::" target="_blank">::FIRSTNAME:: <b>::LASTNAME::</b></a></td><td>::YEAR::</td></tr>'
+
