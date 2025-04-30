4cdfd8d099b9636f3ee207d38382c114bc0f410e
Z L <zian.li.1996@outlook.com>
2025-04-30 23:07:23 +0200

    modify script

--- script/dump-db.sh
+++ script/dump-db.sh
@@ -16,23 +16,84 @@ if [ ! -d ${OUT_DIR} ]; then
   mkdir -p ${OUT_DIR}
 fi
 
+
 # Dump composer list
 echo "var composerlistdata = " > ${OUT_DIR}/composer-data2.js
 
-echo $(sqlite3 ${DBFILE} -json  <<EOF
+echo "$(sqlite3 ${DBFILE} -json <<EOF
 SELECT 
-id,code,firstname,lastname,knownas_name,knownas_name as fullname_ascii,bornyear,diedyear
+  id,
+  code,
+  firstname,
+  lastname,
+  knownas_name,
+  knownas_name as fullname_ascii,
+  bornyear,
+  diedyear
 FROM composers;
 EOF
-) | sed -e 's/{/\n{/g' | sed -e 's/}/\n}/g' | tee -a ${OUT_DIR}/composer-data2.js &> /dev/null
+)" | sed -e 's/{/\n{/g' | sed -e 's/}/\n}/g' | tee -a ${OUT_DIR}/composer-data2.js &> /dev/null
 
+echo ";" | tee -a ${OUT_DIR}/composer-data2.js &> /dev/null
+#LASTCHAR=$(cat ${OUT_DIR}/composer-data2.js | tr -d '[:blank:]' | tr -d '\n' | tail -c 1)
 if [ -f ${OUT_DIR}/composer-data.js ]; then
   rm ${OUT_DIR}/composer-data.js
 fi
 mv ${OUT_DIR}/composer-data2.js ${OUT_DIR}/composer-data.js
 
+
 # Dump works
-#TODO
+echo "var piecelistdata = " > ${OUT_DIR}/piece-data2.js
+echo "$(sqlite3 ${DBFILE} -json  <<EOF
+SELECT
+  id,
+  composer_id,
+  composer_code,
+  arranged,
+  arranger_id,
+  arranger_code,
+  collection_id,
+  collection_code,
+  title,
+  subtitle,
+  subsubtitle,
+  dedicated_to,
+  opus,
+  instrument,
+  folder_hash,
+  comment
+FROM pieces;
+EOF
+)" | sed -e 's/{/\n{/g' | sed -e 's/}/\n}/g' | tee -a ${OUT_DIR}/piece-data2.js &> /dev/null
+
+echo ";" | tee -a ${OUT_DIR}/piece-data2.js &> /dev/null
+if [ -f ${OUT_DIR}/piece-data.js ]; then
+  rm ${OUT_DIR}/piece-data.js
+fi
+mv ${OUT_DIR}/piece-data2.js ${OUT_DIR}/piece-data.js
 
 # Dump collections
-#TODO
+echo "var piecelistdata = " > ${OUT_DIR}/collection-data2.js
+echo "$(sqlite3 ${DBFILE} -json  <<EOF
+SELECT
+  id,
+  code,
+  title,
+  subtitle,
+  subsubtitle,
+  opus,
+  description_text,
+  volume,
+  composer_id,
+  composer_code,
+  instruments,
+  editor
+FROM collections;
+EOF
+)" | sed -e 's/{/\n{/g' | sed -e 's/}/\n}/g' | tee -a ${OUT_DIR}/collection-data2.js &> /dev/null
+
+echo ";" | tee -a ${OUT_DIR}/collection-data2.js &> /dev/null
+if [ -f ${OUT_DIR}/collection-data.js ]; then
+  rm ${OUT_DIR}/collection-data.js
+fi
+mv ${OUT_DIR}/collection-data2.js ${OUT_DIR}/collection-data.js
