ac1a5218d43e5a978e84a01e67d89cd3d8d96146
ZL <zli@cpp.dev>
2026-04-09 21:55:10 -0500

    Add error handling

--- migrate/usecase_display.py
+++ migrate/usecase_display.py
@@ -1,7 +1,9 @@
 from metadata import Metadata
-from flask import render_template
+from filemanager import FileManager
+from flask import render_template, abort
 from config import COMPOSERS_PER_PAGE, COLLECTIONS_PER_PAGE, PIECES_PER_PAGE
 import math
+
 
 def render_composer_list(page=1, items_per_page=COMPOSERS_PER_PAGE):
   composer_list = Metadata().reader().getPartialComposers(items_per_page=items_per_page, page_number=page, listed_only=False)
@@ -42,6 +44,9 @@ def render_composer_piece_list(composer_code, page=1, items_per_page=PIECES_PER_
   total_pieces = Metadata().reader().countComposerPieces(composer_code)
   total_pages = int(math.ceil(total_pieces / float(items_per_page)))
   composer = Metadata().reader().getComposer(composer_code)
+  if not composer:
+    abort(404)
+
   # Get abbr name, e.g. "Johann Sebastian Bach" ==> "J S Bach"
   composer['knownas_name'] = composer.get('knownas_name', 'Unknown')
   abbr_name = [l[0] for l in composer['knownas_name'].split(' ')[:-1]] + [composer['knownas_name'].split(' ')[-1]]
@@ -58,6 +63,8 @@ def render_composer_piece_list(composer_code, page=1, items_per_page=PIECES_PER_
 def render_collection_piece_list(collection_code):
   piece_list = Metadata().reader().getCollectionPieces(collection_code)
   collection = Metadata().reader().getCollection(collection_code)
+  if not collection:
+    abort(404)
 
   composer_name = ""
   if 'composer_code' in collection:
@@ -70,4 +77,5 @@ def render_collection_piece_list(collection_code):
                           piece_list=piece_list, \
                           collection_dict=collection, \
                           composer_name=composer_name)
+
 
